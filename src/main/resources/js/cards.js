c4c6b8d6d8246e3dc5bce624da442851d97c99a2
hejcz <rubin94@gmail.com>
2020-03-19 23:35:24 +0100

    Make cards updatable

--- src/main/resources/js/cards.js
+++ src/main/resources/js/cards.js
@@ -73,7 +73,17 @@ function bounds(points) {
     return {height: maxX - minX + 1, width: maxY - minY + 1};
 }
 
-let currentCard = "10";
+let currentCard = ["14", "10", "07", "13"];
+let index = 0;
+
+setInterval(() => {
+    if (index == 3) {
+        index = 0;
+    } else {
+        index++;
+    }
+    updateCard(cards[currentCard[index]]);
+}, 1000);
 
 const d3Card = d3.select("#current-card")
     .attr("width", 400)
@@ -81,55 +91,82 @@ const d3Card = d3.select("#current-card")
 
 d3Card.append("g")
     .attr("transform", `translate(10 0)`)
+    .attr("class", "title")
     .append("text")
     .attr("x", 0)
-    .attr("y", 15)
-    .text(cards[currentCard].title);
+    .attr("y", 15);
 
 d3Card.append("g")
     .attr("transform", `translate(10 25)`)
-    .attr("class", "terrain")
-    .selectAll("rect")
-    .data(cards[currentCard].terrains)
-    .enter()
-    .append("rect")
-    .attr("width", 30)
-    .attr("height", 30)
-    .attr("transform", function (d, i) { return `translate(${i * 35} 0)`; })
-    .style("stroke-width", "1px")
-    .style("stroke", "black")
-    .style("fill", function (d) { return `rgb(${colorByType[d]})` });
+    .attr("class", "terrain");
 
-const shapeUpdate = d3Card.selectAll(".shape")
-    .data(cards[currentCard].shapes);
+updateCard(cards["10"]);
 
-const shapeEnter = shapeUpdate.enter()
-    .append("g")
-    .attr("transform", function (d, i) { return `translate(${10 + i * 150} ${70})`; })
-    .attr("class", "shape");
+function updateCard(card) {
+    d3.select("#current-card .title text")
+        .text(card.title);
 
-const cardSize = 30;
+    // terrains
+    const terrainUpdate = d3.select("#current-card .terrain")
+        .selectAll("rect")
+        .data(card.terrains);
 
-shapeEnter.selectAll("rect")
-    .data(data => data.points)
-    .enter()
-    .append("rect")
-    .attr("width", cardSize)
-    .attr("height", cardSize)
-    .style("stroke-width", "1px")
-    .style("stroke", "black")
-    .style("fill", "white")
-    .attr("transform", function (d, i) { return `translate(${d.y * cardSize} ${-d.x * cardSize})`; });
+    const terrainEnter = terrainUpdate.enter()
+        .append("rect")
+        .attr("width", 30)
+        .attr("height", 30)
+        .style("stroke-width", "1px")
+        .style("stroke", "black");
 
-// shapes with coins have max width = 2
-shapeEnter.selectAll("circle")
-    .data(data => data.coin ? [bounds(data.points)] : [])
-    .enter()
-    .append("circle")
-    .attr("r", 15)
-    .attr("cx", data => 15 + data.width * cardSize + 10)
-    .attr("cy", data => cardSize * data.height / 2.0)
-    .style("fill", "rgb(255, 230, 0)");
+    terrainUpdate.merge(terrainEnter)
+        .attr("transform", function (d, i) { return `translate(${i * 35} 0)`; })
+        .style("fill", function (d) { return `rgb(${colorByType[d]})` });
+
+    terrainUpdate.exit().remove();
+
+    // shapes
+    const shapeUpdate = d3.select("#current-card").selectAll(".shape")
+        .data(card.shapes);
+
+    shapeUpdate.exit().remove();
+
+    const shapeEnter = shapeUpdate.enter()
+        .append("g")
+        .attr("class", "shape");
+
+    const cardSize = 30;
+
+    shapeUpdate.merge(shapeEnter)
+        .attr("transform", function (d, i) { return `translate(${10 + i * 150} ${70})`; });
+
+    const shapeElementUpdate = shapeUpdate.merge(shapeEnter).selectAll("rect")
+        .data(data => data.points);
+
+    const shapeElementEnter = shapeElementUpdate.enter()
+        .append("rect")
+        .attr("width", cardSize)
+        .attr("height", cardSize)
+        .style("stroke-width", "1px")
+        .style("stroke", "black")
+        .style("fill", "white")
+
+    shapeElementUpdate.exit().remove();
+
+    shapeElementEnter.merge(shapeElementUpdate)
+        .attr("transform", function (d, i) { return `translate(${d.y * cardSize} ${-d.x * cardSize})`; });
+
+    // shapes with coins have max width = 2
+    const coinUpdate = shapeEnter.merge(shapeUpdate).selectAll("circle")
+        .data(data => data.coin ? [bounds(data.points)] : []);
+    const coinEnter = coinUpdate.enter()
+        .append("circle")
+        .attr("r", 15);
+    coinUpdate.merge(coinEnter)
+        .attr("cx", data => 15 + data.width * cardSize + 10)
+        .attr("cy", data => cardSize * data.height / 2.0)
+        .style("fill", "rgb(255, 230, 0)");
+    coinUpdate.exit().remove();
+}
 
 
 //object TreeFortress14 : Card {
