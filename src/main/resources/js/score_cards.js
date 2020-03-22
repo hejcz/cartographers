e5dcd126c32f94b436308144331a843ec4828c56
hejcz <rubin94@gmail.com>
2020-03-22 14:44:09 +0100

    Add missing score cards to front end

--- src/main/resources/js/score_cards.js
+++ src/main/resources/js/score_cards.js
@@ -1,18 +1,67 @@
 const score_cards = {
+    "26": {
+        "title": "LEŚNA WARTA",
+        "description": "Zdobądź 1 punkt za każdy obszar lasu sąsiadujący z krawędzią mapy."
+    },
+    "27": {
+        "title": "ZAGAJNIK",
+        "description": "Zdobądź 1 punkt za każdy rząd i każdą kolumnę z co najmniej jednym obszaren lasu. Ten sam obszar lasu może punktować zarówno w rzędzie jak i kolumnie."
+    },
     "28": {
         "title": "LEŚNA WIEŻA",
-        "description": "Zdobądź 1 gwiazdkę reputacji za każdy obszar lasu otoczony ze wszystkich 4 stron wypełnionymi obszarami lub krawędzią mapy."
+        "description": "Zdobądź 1 punkt za każdy obszar lasu otoczony ze wszystkich 4 stron wypełnionymi obszarami lub krawędzią mapy."
+    },
+    "29": {
+        "title": "GÓRSKIE OSTĘPY",
+        "description": "Zdobądź 3 punkty za każdy obszar gór, połączony z innym obszarem gór przez klaster lasów."
+    },
+    "30": {
+        "title": "POLNY STAW",
+        "description": "Zdobądź 1 punkt za każdy obszar wody sąsiadujący z co najmniej 1 obszarem pola. Zdobądź 1 punkt także za każdy obszar pola sąsiadujący z co najmniej 1 obszarem wody."
+    },
+    "31": {
+        "title": "DOLINA MAGÓW",
+        "description": "Zdobądź 2 punkty za każdy obszar wody sąsiadujący z obszarem gór. Zdobądź 1 punkt za każdy obszar Pola sąsiadujący z obszarem gór."
     },
     "32": {
         "title": "ZŁOTY SPICHLERZ",
-        "description": "Zdobądź 1 gwiazdkę reputacji za każdy obszar wody sąsiadujący z obszarem ruin. Zdobądź 3 gwiazki reputacji takze za każdy obszar Pola na obszarze ruin."
+        "description": "Zdobądź 1 punkt za każdy obszar wody sąsiadujący z obszarem ruin. Zdobądź 3 punkty takze za każdy obszar Pola na obszarze ruin."
+    },
+    "33": {
+        "title": "ROZLEGŁE NABRZEŻE",
+        "description": "Zdobądź 3 punkty za każdy klaster pola nie sąsiadujący z obszarem wody lub krawędzią mapy. Zdobądź 3 punkty za każdy klaster wody, który nie sąsiaduje z obszarem pola lub krawędzią mapy."
+    },
+    "34": {
+        "title": "KOLONIA",
+        "description": "Zdobądź 8 punktów za każdy klaster składający się z 6 lub więcej obszarów wioski."
     },
     "35": {
         "title": "WIELKIE MIASTO",
-        "description": "Zdobądź 1 gwiazdkę reputacji za każdy obszar wioski w największym klastrze wiosek, który nie sąsiaduje z obszarem gór."
+        "description": "Zdobądź 1 punkt za każdy obszar wioski w największym klastrze wiosek, który nie sąsiaduje z obszarem gór."
+    },
+    "36": {
+        "title": "ŻYZNE RÓWNINY",
+        "description": "Zdobądź 3 punkty za każdy klaster obszarów Wioski, który sąsiaduje z 3 lub więcej różnymi rodzajami obszarów."
+    },
+    "37": {
+        "title": "UMOCNIENIA",
+        "description": "Zdobądź 2 punkty za każdy obszar wioski w drugim co do wielkości klastrze obszarów wioski."
+    },
+    "38": {
+        "title": "POGRANICZE",
+        "description": "Zdobądź 6 punktów za każdy kompletny rząd lub kolumnę wypełnionych obszarów."
+    },
+    "39": {
+        "title": "UTRACONE WŁOŚCI",
+        "description": "Zdobądź 3 punkty za każdy obszar wzdłóż jednej krawędzi największego kwadratu złożonego z wypełnionych obszarów."
     },
     "40": {
         "title": "TRAKT HANDLOWY",
-        "description": "Zdobądź 3 gwiazdki reputacji za każdą kompletną ukośną linię z wypełnionych obszarów, która łączy LEWĄ I DOLNĄ krawędź mapy."
-    }
+        "description": "Zdobądź 3 punkty za każdą kompletną ukośną linię z wypełnionych obszarów, która łączy LEWĄ I DOLNĄ krawędź mapy."
+    },
+    "41": {
+        "title": "KRYJÓWKI",
+        "description": "Zdobądź 1 punkt za każdy pusty obszar otoczony ze wszystkich 4 stron wypełnionymi obszarami lub krawędzią mapy."
+    },
+
 };
